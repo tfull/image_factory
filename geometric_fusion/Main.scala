98f16ee620982acfc9567aae3f2961038b4bec5e
tfull <tfurukawa.mail@gmail.com>
2020-02-04 23:04:08 +0900

    + file

---
+++ geometric_fusion/Main.scala
@@ -0,0 +1,10 @@
+object Main {
+}
+
+case class Color(r: Int, g: Int, b: Int) {
+    def +(that: Color): Color = {
+        Color(this.r + that.r, this.g + that.g, this.b + that.b)
+    }
+
+    override def toString() = "rgb(" + r.toString() + ", " + g.toString() + ", " + b.toString() + ")"
+}
